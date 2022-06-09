3f9e392644796ed8a3f252d7d895e378b6476b6a
Rajibul <rajibridoy884@gmail.com>
2022-06-09 08:38:05 +0600

    Update anemometer.py

--- anemometer.py
+++ anemometer.py
@@ -1,9 +1,10 @@
-from machine import Pin
 from config import config
+from math import pi
 
 
 class Anemometer:
     def __init__(self):
+        self.l = 20  # arm length (cm)
         self.dic = {
             0b000: "N",
             0b100: "NE",
@@ -17,7 +18,7 @@ class Anemometer:
         self.t = None
         self.speed = 0.0
         self.cardinal = "N"
-    
+
     def update(self):
         try:
             t = config.i2c.readfrom(config.arduino_addr, 5)
@@ -25,14 +26,16 @@ class Anemometer:
             self.speed = 0.0
             self.cardinal = "N"
             return
-        # self.speed = (t[0] << 8 | t[1] << 8 | t[2]) << 8 | t[3]
+        c = t[0] << 8 | t[1]  # rotation count
+        d = t[2] << 8 | t[3]  # timed for (ms)
+        self.speed = self.l*c*pi*72/d
         self.cardinal = self.dic[t[4]]
 
     # @property
     # def cardinal(self):
     #     # return self.dic[((not self.ir_1.value()) << 1 | (not self.ir_2.value())) << 1 | (not self.ir_3.value())]
     #     return self.dic[self.t[4]]
-    
+
     # @property
     # def speed(self):
     #     return ((t[0] << 8 | t[1] << 8 | t[2]) << 8 | t[3]) << 8 | t[4]
