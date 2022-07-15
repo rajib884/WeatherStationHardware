80804a24574bf3bdf19a519fc3085a864b91002f
Rajibul <rajibridoy884@gmail.com>
2022-07-16 00:31:18 +0600

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,21 +1,27 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-# import esp
-# esp.osdebug(None)
-import uos as os
 
-import machine
-
-# uos.dupterm(None, 1) # disable REPL on UART(0)
-
-machine.freq(240000000)
 import gc
 
-# import webrepl
-# webrepl.start()
-gc.collect()
-from config import config
-import sdcard
+import machine
+import uos as os
 
-sd = sdcard.SDCard(config.spi, machine.Pin(config.cs_sd))
-os.mount(sd, '/sd')
+from config import config
+
+# import webrepl
+# import esp
+# esp.osdebug(None)
+
+machine.freq(240000000)
 gc.collect()
+
+# webrepl.start()
+# uos.dupterm(None, 1) # disable REPL on UART(0)
+
+if config.save_to_sdcard:
+    import sdcard
+
+    # sd = sdcard.SDCard(config.spi, machine.Pin(config.cs_sd), baudrate=18000000)
+    sd = sdcard.SDCard(config.spi, machine.Pin(config.cs_sd))
+    os.mount(sd, '/sd')
+    print("SD Card Mounted")
+    gc.collect()
