4926b896f7348f6ec9c01eaedc65f22a809f94fa
Rajibul <rajibridoy884@gmail.com>
2022-08-12 19:18:04 +0600

    Update anemometer.py

--- anemometer.py
+++ anemometer.py
@@ -1,3 +1,4 @@
+import time
 from config import config
 from math import pi
 
@@ -5,31 +6,26 @@ from math import pi
 class Anemometer:
     def __init__(self):
         self.l = 20  # arm length (cm)
-        self.dic = {
-            0b000: "N",
-            0b100: "NE",
-            0b101: "E",
-            0b001: "SE",
-            0b011: "S",
-            0b111: "SW",
-            0b110: "W",
-            0b010: "NW",
-        }
-        self.t = None
+        self.directions = ["N", "NE", "E","SE", "S","SW","W","NW"]
         self.speed = 0.0
         self.cardinal = "N"
+        self.last_reading = None
 
     def update(self):
+
         try:
-            t = config.i2c.readfrom(config.arduino_addr, 5)
+            t = config.i2c.readfrom(config.arduino_addr, 3)
+            self.cardinal = self.directions[t[2]]
+            if self.last_reading is None:
+                self.speed = 0.0
+            else:
+                c = t[0] << 8 | t[1]  # rotation count
+                self.speed = self.l*c*pi*9/(time.ticks_diff(time.ticks_ms(), self.last_reading))
+            self.last_reading = time.ticks_ms()
         except:
             self.speed = 0.0
             self.cardinal = "N"
-            return
-        c = t[0] << 8 | t[1]  # rotation count
-        d = t[2] << 8 | t[3]  # timed for (ms)
-        self.speed = self.l*c*pi*72/d
-        self.cardinal = self.dic[t[4]]
+            self.last_reading = None
 
     # @property
     # def cardinal(self):
